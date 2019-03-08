237775c4085cee352028212993dd28cad608d85a
Akshay Arora <luciferx86@Akshays-MacBook-Pro.local>
2019-03-08 21:32:25 +0530

    added driver login and basic map view

--- app/src/main/java/com/alchemists/silicon_app/Ambulance.java
+++ app/src/main/java/com/alchemists/silicon_app/Ambulance.java
@@ -1,9 +1,18 @@
 package com.alchemists.silicon_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Ambulance extends AppCompatActivity {
+
+    public void loginUser(View view){
+        Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(i);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

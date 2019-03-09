d45d87b9a21fa43b9497efa46bd994008b00c769
Akshay Arora <luciferx86@Akshays-MacBook-Pro.local>
2019-03-09 10:25:22 +0530

    Sending Data to server

--- app/src/main/java/com/alchemists/silicon_app/User.java
+++ app/src/main/java/com/alchemists/silicon_app/User.java
@@ -4,9 +4,98 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class User extends AppCompatActivity {
+
+    public OkHttpClient client;
+
+    EditText email;
+    EditText password;
+
+    Button regiser ;
+
+    JSONObject student1;
+
+    public void sendData(){
+        String emailVal = email.getText().toString();
+        String passVal = password.getText().toString();
+
+        student1 = new JSONObject();
+        try {
+            student1.put("email", emailVal);
+            student1.put("password", passVal);
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    private final class EchoWebSocketListener extends WebSocketListener {
+        private static final int NORMAL_CLOSURE_STATUS = 1000;
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            String emailvar = "Silicon@alchemist.com";
+            String passvar = "passsssss";
+            String emailVal = email.getText().toString();
+            String passVal = password.getText().toString();
+            JSONObject student1 = new JSONObject();
+            try {
+                student1.put("email", emailVal);
+                student1.put("password", passVal);
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            webSocket.send(student1.toString());
+
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+//            output("Receiving : " + text);
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+//            output("Receiving bytes : " + bytes.hex());
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+//            output("Closing : " + code + " / " + reason);
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+//            output("Error : " + t.getMessage());
+        }
+    }
+    private void start() {
+        Request request = new Request.Builder().url("ws://10.177.7.176:3006/signinws").build();
+        User.EchoWebSocketListener listener = new User.EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket(request, listener);
+        client.dispatcher().executorService().shutdown();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +114,19 @@ public class User extends AppCompatActivity {
                 startActivity(RegisterIntent);
             }
         });
+        regiser = findViewById(R.id.registerBtn);
+        regiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+
+
+        client = new OkHttpClient();
+
+
+        email = findViewById(R.id.emailVal);
+        password = findViewById(R.id.passVal);
     }
 }
